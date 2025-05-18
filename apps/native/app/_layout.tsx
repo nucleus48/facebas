931e7c1c9868b6b2906e25dc3fd7ca37a7873b08
nucleus48 <nucleuserumagborie@gmail.com>
2025-05-18 14:34:08 +0100

    native: packages and project configuration

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,6 +1,39 @@
 import "@/global.css";
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_600SemiBold,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_600SemiBold,
+    Roboto_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hide();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
--- apps/native/tailwind.config.js
+++ apps/native/tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
+    fontFamily: {
+      "roboto-400": ["Roboto_400Regular"],
+      "roboto-500": ["Roboto_500Medium"],
+      "roboto-600": ["Roboto_600SemiBold"],
+      "roboto-700": ["Roboto_700Bold"],
+    }
   },
   plugins: [],
 };
